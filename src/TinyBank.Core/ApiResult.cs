f90064e5f5ca11bfc578577a5a1786b0c3676a8b
dpnevmatikos <pnevmatikos@gmail.com>
2021-03-19 09:20:56 +0200

    Introduce ApiResult

---
+++ src/TinyBank.Core/ApiResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyBank.Core
+{
+    public class ApiResult<T>
+    {
+        public int Code { get; set; }
+        public string ErrorText { get; set; }
+        public T Data { get; set; }
+
+        public ApiResult()
+        {
+            Code = Constants.ApiResultCode.Success;
+        }
+
+        public bool IsSuccessful()
+        {
+            return Code == Constants.ApiResultCode.Success;
+        }
+    }
+}
