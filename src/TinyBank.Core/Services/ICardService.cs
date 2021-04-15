5543d97aba8fddaf77f09fd9332663e66b14b495
kparginos <kparginos@yahoo.gr>
2021-04-15 16:59:40 +0300

    Create Cards Service

---
+++ src/TinyBank.Core/Services/ICardService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+using TinyBank.Core.Model;
+using TinyBank.Core.Services.Options;
+
+namespace TinyBank.Core.Services
+{
+    public interface ICardService
+    {
+        public ApiResult<Card> Register(CreateCardOptions options);
+        public ApiResult<Card> BindCardToAccount(string cardNumber, string accontNumber);
+        public ApiResult<Card> GetCardbyNumber(string cardNumber);
+        public IQueryable<Card> Search(SearchCardOption options);
+    }
+}
